a90dee36fc3912ece5f77a4cf3c5aaad00873c53
paples0 <125643067+klikate@users.noreply.github.com>
2023-09-15 10:19:16 +0300

    Practice1

---
+++ control_practice1/src/main/java/ru/mirea/klimova/control_practice1/SecondActivity.java
@@ -0,0 +1,21 @@
+package ru.mirea.klimova.control_practice1;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SecondActivity extends AppCompatActivity {
+
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        button = findViewById(R.id.button1);
+    }
+}
